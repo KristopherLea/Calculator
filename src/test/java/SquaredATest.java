7341d484338b16f51c7509ea87e98f00a5474e7d
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-09 15:10:42 -0600

    added 3 more tests

--- src/test/java/SquaredATest.java
+++ src/test/java/SquaredATest.java
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ThirdTest {
+public class SquaredATest {
 
     @Test
-    public void multiply(){
+    public void squaredA(){
         int expected = 0;
-        int actual = Calculator.multiply();
+        int actual = Calculator.squaredA();
         assertEquals(expected, actual);
     }
 }
